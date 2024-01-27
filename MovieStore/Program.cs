2a271225d04518a13b4478e39ddf3930eef7e04e
Arda Şen <ardasen.96@gmail.com>
2024-01-27 17:35:02 +0300

    fix: CORS policy

--- MovieStore/Program.cs
+++ MovieStore/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAnyOrigin",
         builder =>
         {
-            builder.WithOrigins("http://localhost:3000");
+            builder.WithOrigins("https://movie-front-plum.vercel.app");
             builder.AllowAnyHeader();
             builder.AllowAnyMethod();
             builder.AllowCredentials();
